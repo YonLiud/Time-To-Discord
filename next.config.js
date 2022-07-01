94cb9e418b7637a81437bba94970be18606dceb9
YonLiud <ejliud@gmail.com>
2022-07-01 18:06:08 +0300

    preparations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'cdn.yonchukku.dev',
+      'www.yonchukku.dev',
+      'yonchukku.dev',
+      'cdn.vercel.app'
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -13,46 +13,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Time Converter to Discord Unix Timestamp
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
+        <a
+          href="https://www.yonchukku.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built By ‎<b> Yonchukku{' '}</b>
+          <span className={styles.logo}>
+            <Image src="https://www.yonchukku.dev/_next/image?url=%2FFlower.png&w=64&q=64" alt="Vercel Logo" width={16} height={16} />
+          </span>
+        </a>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
